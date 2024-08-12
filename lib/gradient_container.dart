23fa18489433b9c9290b63b1a03eb4f83fd00fd2
lightrao <lightrao@hotmail.com>
2024-08-13 07:45:15 +0800

    Displaying Images & Using Multiple Constructor Functions

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_first_flutter_app/styled_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
@@ -20,8 +19,11 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText("Hello World!"),
+      child: Center(
+        child: Image.asset(
+          "assets/images/dice-2.png",
+          width: 200,
+        ),
       ),
     );
   }
